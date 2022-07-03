3e997cbf359b7171ac4a40715ed32491eb4e0102
우빈 <102154880+Ubinquitous@users.noreply.github.com>
2022-07-03 19:13:48 +0900

    22-07-03 clone-momentum

--- clone-momentum/src/JS/background.js
+++ clone-momentum/src/JS/background.js
@@ -1,6 +1,6 @@
 const images = [ "background-1.jpg","background-2.jpg","background-3.jpg","background-4.jpg",
                  "background-5.jpg","background-6.jpg","background-7.jpg","background-8.jpg",
-                 "background-9.jpg","background-10.jpg","background-11.jpg","background-12.jpg"]
+                 "background-9.jpg","background-10.jpg","background-11.jpg","background-12.jpg"];
 
 let randomImage = Math.round(Math.random()*images.length);
 
